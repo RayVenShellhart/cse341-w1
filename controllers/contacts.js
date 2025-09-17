b526ce73713d1d438ef17286723bc822f021fc92
RayVenShellhart <rayvenshellhart@gmail.com>
2025-09-17 15:54:18 -0700

    error-handling

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -3,8 +3,15 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAll = async (req, res) => {
     //#swagger.tag=['Contacts']
-    const result = await mongodb.getDatabase().db().collection('Contacts').find()
-    result.toArray().then((contacts) => {
+    mongodb
+        .getDatabase()
+        .db()
+        .collection('Contacts')
+        .find()
+        .toArray((err, contacts) => {
+        if (err) {
+            res.status(400).json({ message: err })
+        }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts)
     });
@@ -12,9 +19,19 @@ const getAll = async (req, res) => {
 
 const getSingle = async (req, res) => {
     //#swagger.tag=['Contacts']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use valid contact id to find a contact')
+    }
     const contactId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().db().collection('Contacts').find({ _id: contactId });
-    result.toArray().then((contacts) => {
+    mongodb
+        .getDatabase()
+        .db()
+        .collection('Contacts')
+        .find({ _id: contactId })
+        .toArray((err, contacts) => {
+            if (err) {
+                res.status(400).json({ message: err })
+            }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(contacts[0]);
     });
@@ -40,6 +57,9 @@ const createContact = async (req, res) => {
 
 const updateContact = async (req, res) => {
     //#swagger.tag=['Contacts']
+    if (!ObjectId.isValid(req.params.id)) {
+        res.status(400).json('Must use valid id to update')
+    }
     const contactId = new ObjectId(req.params.id)
     const contact = {
         firstName: req.body.firstName,
@@ -52,18 +72,21 @@ const updateContact = async (req, res) => {
     if (response.modifiedCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || "Soem error has occured while updating the user.")
+        res.status(500).json(response.error || "Some error has occured while updating the contact.")
     }
 };
 
 const deleteContact = async (req, res) => {
     //#swagger.tag=['Contacts']
+    if (!ObjectId.isValid(req.params.id)) {
+    res.status(400).json('Must use a valid contact id to delete a contact.');
+    }
     const contactId = new ObjectId(req.params.id);
     const response = await mongodb.getDatabase().db().collection('Contacts').deleteOne({ _id: contactId });
     if (response.deleteCount > 0) {
         res.status(204).send();
     } else {
-        res.status(500).json(response.error || "Soem error has occured while updating the user.")
+        res.status(500).json(response.error || "Soem error has occured while updating the contact.")
     }
 };
 
